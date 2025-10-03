70c535e12213775386b72f63b26f71b472ee199f
Mholi Khayn <khayn@gmail.com>
2025-10-03 13:43:24 +0200

    Add ExternalModel class to external_model.py

---
+++ models/external_model.py
@@ -0,0 +1,9 @@
+# models/external_model.py
+
+class ExternalModel:
+    def __init__(self, params):
+        self.params = params
+
+    def predict(self):
+        # Placeholder logic
+        return {"home": 1.2, "away": 0.8}
